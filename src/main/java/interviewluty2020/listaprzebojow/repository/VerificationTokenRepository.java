b31186b97694f623dc885c7d940c7eba673bbda4
STEFAN <stefan.jan.p@gmail.com>
2021-02-01 06:19:00 +0100

    Properties

--- src/main/java/interviewluty2020/listaprzebojow/repository/VerificationTokenRepository.java
+++ src/main/java/interviewluty2020/listaprzebojow/repository/VerificationTokenRepository.java
@@ -8,7 +8,4 @@ import java.util.Optional;
 public interface VerificationTokenRepository extends JpaRepository<VerificationToken, Long>
 {
     Optional<VerificationToken> findByToken(String token);
-
-
-
 }
